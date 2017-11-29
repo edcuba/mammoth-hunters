0773b7fd744c1d737a2f534cadf0e26c511b87cc
H00N24 <ondrej.kurak.h@gmail.com>
2017-11-29 16:42:47 +0100

    hunter profile, hunter list

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
 from .models import Hunter
 
 
@@ -6,4 +7,16 @@ class HunterCreationForm(UserCreationForm):
 
     class Meta:
         model = Hunter
-        fields = ['username', 'password1', 'password2', 'first_name', 'last_name', 'age']
+        fields = ['username', 'password1', 'password2', 'first_name', 'last_name']
+
+class HunterChangeForm(ModelForm):
+    class Meta:
+        model = Hunter
+        fields = ['username', 'first_name', 'last_name', 'role',
+                  'Strength', 'Stamina', 'Agility', 'Intellect', 'Speed']
+    
+    def __init__(self, *args, **kwargs):
+        super(HunterChangeForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['readonly'] = True
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
--- app/hunter/views.py
+++ app/hunter/views.py
@@ -1,37 +1,83 @@
 from django.shortcuts import render
 from ..models import Hunter, ROLES, HunterAbilities, Watch, Hunt
+from ..forms import HunterChangeForm
 
 def profile(request):
-    c = {}
+    cont = {}
     tmp = request.GET.get('id_hunter', request.user.id)
     hunter = Hunter.objects.get(pk=tmp)
-    """
-    if request.method == 'GET':
-        print(request.POST['id_hunter'])
-        hunter = Hunter.objects.get(pk=request.POST['id_hunter'])
-    else:
-        hunter = Hunter.objects.get(pk=request.user.id)
-"""
     skills = HunterAbilities.objects.filter(hunter=hunter.id)
-    c['role_name'] = ROLES[hunter.role][1]
+    hunter.role_name = ROLES[hunter.role][1]
     location = False
+    act = ''
     try:
         location = Watch.objects.get(hunters=hunter.id, active=True).location
-    finally:
+        act = 'Watching'
+    except:
         pass
-    '''
     try:
         location = Hunt.objects.get(hunter=hunter.id, finished=False).started_by.location
-    finally:
+        act = 'Hunting'
+    except:
         pass
-    '''
+
     if not location:
         location = 'Cave'
-    c['location'] = location
-    c['hunter'] = hunter
-    return render(request, 'app/hunter/profile.html', c)
+    else:
+        location = '{0} ({1})'.format(location, act)
+    hunter.location = location
+    if hunter.killedBy:
+        hunter.alive = 'Dead'
+    else:
+        hunter.alive = 'Alive'
+    cont['hunter'] = hunter
+    try:
+        cont['us'] = Hunter.objects.get(pk=request.user.id).role
+    except:
+        cont['us'] = 2
+    
+    cont['form'] = HunterChangeForm(instance=hunter)
+    if cont['us'] != 2:
+        for field in cont['form'].fields.values():
+            field.widget.attrs['readonly'] = True
+    if request.method == 'POST':
+        cont['form'] = HunterChangeForm(request.POST, instance=hunter)
+        if cont['form'].is_valid():
+            hunter = cont['form'].save()
+    
+    return render(request, 'app/hunter/profile.html', cont)
 
 def hunterList(request):
-    context = {}
-    context['hunters'] = Hunter.objects.all()
-    return render(request, 'app/hunter/list.html', context)
+    cont = {}
+    cont['hunters'] = Hunter.objects.all()
+    for hunter in cont['hunters']:
+        location = False
+        act = ''
+        try:
+            location = Watch.objects.get(hunters=hunter.id, active=True).location
+            act = 'Watching'
+        except:
+            pass
+        try:
+            location = Hunt.objects.get(hunter=hunter.id, finished=False).started_by.location
+            watch = False
+            act = 'Hunting'
+        except:
+            pass
+        if not location:
+            location = 'Cave'
+        else:
+            location = '{0} ({1})'.format(location, act)
+        hunter.location = location
+        hunter.role_name = ROLES[hunter.role][1]
+        if hunter.killedBy:
+            hunter.alive = 'Dead'
+        else:
+            hunter.alive = 'Alive'
+        hunter.score = round((hunter.Strength +
+                              hunter.Stamina +
+                              hunter.Agility +
+                              hunter.Intellect +
+                              hunter.Speed)/100, 1)
+    cont['stars'] = list(range(1, 6))
+    return render(request, 'app/hunter/list.html', cont)
