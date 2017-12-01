ae7017abe47b1640d43a4296c81528b2c09cb64e
H00N24 <ondrej.kurak.h@gmail.com>
2017-12-01 14:20:29 +0100

    watch query

--- app/watch/forms.py
+++ app/watch/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
-from ..models import Watch
+from ..models import Watch, Hunter, Hunt
+from django.db.models import Q
 
 class WatchForm(ModelForm):
     class Meta:
@@ -8,6 +9,10 @@ class WatchForm(ModelForm):
     
     def __init__(self, *args, **kwargs):
         super(ModelForm, self).__init__(*args, **kwargs)
+        watches = Watch.objects.filter(active=True)
+        hunts = Hunt.objects.filter(finished=False)
+        hunters = Hunter.objects.filter(~Q(watch__in=watches) & ~Q(hunt__in=hunts))
+        self.fields['hunters'].queryset = hunters
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
 
