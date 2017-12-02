620838631542addf3e6ac840dc3a8e7266b141eb
Eduard Čuba <ecuba@redhat.com>
2017-12-02 19:17:54 +0100

    introduce pnotify

--- app/hunter/urls.py
+++ app/hunter/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import profile, hunterList
+from .views import profile, hunterList, changePass
 
 urlpatterns = [
     url(r'^$', profile, name='hunter_profile'),
+    url(r'^password', changePass, name='hunter_password'),
     url(r'^list', hunterList, name='hunter_list')
 ]
