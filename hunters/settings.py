d5e9e381f3e52925f765abcce0235546ecc6136e
H00N24 <ondrej.kurak.h@gmail.com>
2017-12-03 17:21:11 +0100

    deploy ready

--- hunters/settings.py
+++ hunters/settings.py
@@ -25,9 +25,9 @@ SITE_NAME = basename(DJANGO_ROOT)
 SECRET_KEY = '8*md2t)o**67@*yhc(d=f@j95kl(dnf^rmm4s00$-mh_vurb2b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'http://h00n24.pythonanywhere.com/']
 
 # Application definition
 
