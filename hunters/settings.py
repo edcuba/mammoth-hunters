594200506b641cbac249dc6e95d229bea1edeb28
Eduard Čuba <ecuba@redhat.com>
2017-12-04 09:36:23 +0100

    edit doc and enable debug mode

--- hunters/settings.py
+++ hunters/settings.py
@@ -25,7 +25,7 @@ SITE_NAME = basename(DJANGO_ROOT)
 SECRET_KEY = '8*md2t)o**67@*yhc(d=f@j95kl(dnf^rmm4s00$-mh_vurb2b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'h00n24.pythonanywhere.com']
 
